0c69e9be61d8aeee7ea0590d81a604a5bc79b1ab
BM33let <amnesiablyay@gmail.com>
2025-04-27 12:45:40 +0300

    Dashboard changed new

--- BOROMOTORS/Models/DashboardViewModel.cs
+++ BOROMOTORS/Models/DashboardViewModel.cs
@@ -7,6 +7,7 @@ namespace BOROMOTORS.Models
         public int TotalBikes { get; set; }
         public decimal AveragePrice { get; set; }
         public decimal CheapestBike { get; set; }
+        public decimal LowestPrice { get; set; }
         public string TopManufacturer { get; set; }
     }
 }
